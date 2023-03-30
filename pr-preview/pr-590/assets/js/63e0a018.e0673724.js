9d8ce416db416653b237d8ab45c8f249cd07c89e
jcstein <jcstein@users.noreply.github.com>
2023-03-30 12:30:59 +0000

    Remove preview for PR 590 🛬

--- pr-preview/pr-590/assets/js/63e0a018.e0673724.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcelestia_docs=self.webpackChunkcelestia_docs||[]).push([[2066],{879:e=>{e.exports=JSON.parse('{"title":"How Celestia works","slug":"/category/how-celestia-works","permalink":"/pr-preview/pr-590/category/how-celestia-works","navigation":{"next":{"title":"Introduction","permalink":"/pr-preview/pr-590/concepts/how-celestia-works/introduction"}}}')}}]);

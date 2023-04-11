869cf4e528049c33c627959846f39ce72b7b18f6
YazzyYaz <YazzyYaz@users.noreply.github.com>
2023-04-11 12:19:27 +0000

    Deploy preview for PR 645 🛫

---
+++ pr-preview/pr-645/assets/js/3d9f1156.fc0a2562.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcelestia_docs=self.webpackChunkcelestia_docs||[]).push([[6013],{351:e=>{e.exports=JSON.parse('{"title":"How Celestia works","slug":"/category/how-celestia-works","permalink":"/pr-preview/pr-645/category/how-celestia-works","navigation":{"next":{"title":"Introduction","permalink":"/pr-preview/pr-645/concepts/how-celestia-works/introduction"}}}')}}]);
